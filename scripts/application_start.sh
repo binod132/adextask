877066725942787e45cdb603e69c9beef0c1cad4
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-02-12 15:35:59 +0545

    appspec

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 232048837608.dkr.ecr.us-east-1.amazonaws.com/adex-ecr:latest
+sudo docker run --name flask_app -d -p 5000:5000 232048837608.dkr.ecr.us-east-1.amazonaws.com/adex-ecr:latest
